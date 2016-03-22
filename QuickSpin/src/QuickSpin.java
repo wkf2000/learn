5616a370c281c0028ce085da722bf78192cfd952
wkf2000 <wkf2000@hotmail.com>
2016-03-21 20:54:06 -0700

    quick spin

--- QuickSpin/src/QuickSpin.java
+++ QuickSpin/src/QuickSpin.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by kefei on 3/21/16.
@@ -7,8 +6,25 @@ import java.util.List;
 public class QuickSpin {
     public static void main(String [] args)
     {
-        final List<String> comm = Arrays.asList("1", "2", "3", "22", "4", "23", "4", "5", "22","6", "23","7", "8");
+        final List<String> comm = new ArrayList<>(
+                Arrays.asList("1", "2", "3", "22", "4", "23", "4", "5", "22","6", "23","7", "8"));
+        System.out.println("original: " + comm);
 
-        System.out.println(comm);
+
+        //1. iterator
+        Set<String> ipset = new LinkedHashSet<>();
+        ListIterator<String> iter = comm.listIterator();
+        while(iter.hasNext()){
+            String s  = iter.next();
+            if(s.contains("2")) {
+                if (!ipset.add(s)) {
+                    iter.remove();
+                }
+            }
+        }
+        System.out.println("result1: " + comm);
+
+        //2.
+
     }
 }
