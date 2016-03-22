9671717caf1b381a80e634b819bbc6a0aed9d859
wkf2000 <wkf2000@hotmail.com>
2016-03-21 21:06:17 -0700

    quick spin

--- QuickSpin/src/QuickSpin.java
+++ QuickSpin/src/QuickSpin.java
@@ -1,4 +1,7 @@
+import com.google.common.collect.ImmutableSet;
+
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by kefei on 3/21/16.
@@ -24,7 +27,18 @@ public class QuickSpin {
         }
         System.out.println("result1: " + comm);
 
-        //2.
+        //2. guava, it preserves order :)
+        List<String> result = ImmutableSet.copyOf(comm).asList();
+        System.out.println("result2: " + result);
+
+
+        //3. java8
+        TreeSet<String> result3 = comm.stream()
+                .collect(Collectors.toCollection(() ->
+                                new TreeSet<>((p1, p2) -> p1.compareTo(p2))
+                ));
+
+        System.out.println("result3: " + result3);
 
     }
 }
