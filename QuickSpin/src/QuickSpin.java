f7ff752d49f8ec4f847a72c6c1a1b20416a65551
wkf2000 <wkf2000@hotmail.com>
2016-03-21 20:15:38 -0700

    quick spin

---
+++ QuickSpin/src/QuickSpin.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by kefei on 3/21/16.
+ */
+public class QuickSpin {
+    public static void main(String [] args)
+    {
+        final List<String> comm = Arrays.asList("1", "2", "3", "22", "4", "23", "4", "5", "22","6", "23","7", "8");
+
+        System.out.println(comm);
+    }
+}
